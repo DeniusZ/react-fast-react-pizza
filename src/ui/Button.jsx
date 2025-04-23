2bab89252cb91b6ffb89b5b09ce2b9b11ca3e279
Deniss Zubkovs <deniss.zubkovs@issuu.com>
2025-04-23 08:28:42 +0200

    add redux

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ const base =
   'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50';
 
 // eslint-disable-next-line react/prop-types
-function Button({ children, disabled, to, type = 'primary' }) {
+function Button({ children, disabled, to, type = 'primary', onClick }) {
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
@@ -17,6 +17,14 @@ function Button({ children, disabled, to, type = 'primary' }) {
         {children}
       </Link>
     );
+
+  if (onClick) {
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
 
   return (
     <button disabled={disabled} className={styles[type]}>
